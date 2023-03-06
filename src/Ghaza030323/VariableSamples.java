43d7aa92ceeeebba70a13c7209791c8cf7c4813a
MuhammadGhazaFadillah <126837416+MuhammadGhazaFadillah@users.noreply.github.com>
2023-03-06 08:38:38 +0700

    first commit

---
+++ src/Ghaza030323/VariableSamples.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ghaza030323;
+
+/**
+ *
+ * @author muham
+ */
+public class VariableSamples {
+    public static void main(String[] args) {
+        //deklarasi a tipe data dengan nama variable
+        //result dan tipe data boolean
+        boolean result;
+        
+        //deklarasi tipe data dengan nama variable
+        //grade, double tipe data dan telah di inisialisasi
+        //to 0.0
+        double grade = 0.0;
+    }
+}
